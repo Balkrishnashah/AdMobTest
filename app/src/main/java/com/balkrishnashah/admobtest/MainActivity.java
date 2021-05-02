aebf035c3a24a34235de670dce7b6ec38173fa8f
Balkrishnashah <shahkrishna493@gmail.com>
2021-05-02 11:45:30 +0530

    Initial commit

---
+++ app/src/main/java/com/balkrishnashah/admobtest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.balkrishnashah.admobtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = new AdView(this);
+
+        mAdView.setAdSize(AdSize.BANNER);
+
+        mAdView.setAdUnitId("ca-app-pub-2903696734123870/8464735572");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                Toast.makeText(getApplicationContext(), "error : "+adError, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
